e4a25111b0fdca5bf4c55d5733fe5c8ba07c47dd
hjk7 <hjk7@sfu.ca>
2015-02-11 14:58:13 -0800

    yoink

---
+++ scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name testApp
+ * @description
+ * # testApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('testApp', [
+    'ngAnimate',
+    'ngMessages',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'gridster'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ scripts/controllers/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testApp
+ */
+angular.module('testApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+  $scope.gridsterOpts = {
+    margins: [10, 10],
+    columns: 5,
+    outerMargin: false,
+    pushing: true,
+    floating: true,
+    draggable: {
+      enabled: true
+    },
+    resizable: {
+      enabled: true,
+      handles: ['n', 'e', 's', 'w', 'se', 'sw']
+    }
+  };
+
+    $scope.standardItems = [
+      { sizeX: 1, sizeY: 1, row: 0, col: 0 },
+      { size: { x: 1, y: 2 }, position: [0, 2] },
+      { size: { x: 1, y: 1 }, position: [0, 2] },
+      { size: { x: 1, y: 1 }, position: [0, 2] }
+    ];
+
+
+
+  });
