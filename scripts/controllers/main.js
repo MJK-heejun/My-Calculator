da32f9fd2f0bca1539974337c5ef2cd9b2881a55
hjk7 <hjk7@sfu.ca>
2015-02-13 01:41:55 -0800

    serialize, deserialize, save, reset implemented

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -9,11 +9,6 @@
  */
 angular.module('testApp')
   .controller('MainCtrl', function ($scope, snapRemote) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
 
     $scope.snapOpts = {
       disable: 'right',
@@ -22,10 +17,10 @@ angular.module('testApp')
 
     $scope.gridsterOpts = {
       margins: [10, 10],
-      columns: 5,
+      columns: 10,
       outerMargin: false,
       pushing: true,
-      floating: false,
+      floating: true,
       mobileBreakPoint: 330,
       draggable: {
         enabled: true
@@ -37,24 +32,77 @@ angular.module('testApp')
     };
 
 
-    $scope.standardItems = [
-      { sizeX: 1, sizeY: 1, row: 0, col: 0 },
-      { size: { x: 1, y: 2 }, position: [0, 2] },
-      { size: { x: 1, y: 1 }, position: [0, 2] },
-      { size: { x: 1, y: 1 }, position: [0, 2] }
+    $scope.default_items = [
+      { sizeX: 1, sizeY: 1, row: 0, col: 0, name: "sss" },
+      { size: { x: 1, y: 2 }, position: [0, 2], name: "sss2" },
+      { size: { x: 1, y: 1 }, position: [0, 2], name: "sss3" },
+      { size: { x: 1, y: 1 }, position: [0, 2], name: "sss4"  }
     ];
 
 
-    /*
-    snapRemote.getSnapper().then(function(snapper) {
-      snapper.on('open', function() {
-        console.log('Drawer opened!');
-      });
-      
-      snapper.on('close', function() {
-        console.log('Drawer closed!');
-      });
-    });
-*/
+    //initiate the current items that will be displayed
+    $scope.current_items = [];
+    if(localStorage.getItem("saved_items") == null){
+      $scope.current_items = $scope.default_items;
+    }else{
+      $scope.current_items = angular.fromJson(localStorage.getItem("saved_items"));
+    }
+
+
+
+
+
+    //adding element
+    $scope.default_items.push({ size: { x: 1, y: 1 }, position: [0, 0], name: "new" });
+
+    $scope.dashboards = {
+      '1': {
+        id: '1',
+        name: 'Home',
+        widgets: [{
+          col: 0,
+          row: 0,
+          sizeY: 1,
+          sizeX: 1,
+          name: "Widget 1"
+        }, {
+          col: 2,
+          row: 1,
+          sizeY: 1,
+          sizeX: 1,
+          name: "Widget 2"
+        }]
+      },
+      '2': {
+        id: '2',
+        name: 'Other',
+        widgets: [{
+          col: 1,
+          row: 1,
+          sizeY: 1,
+          sizeX: 2,
+          name: "Other Widget 1"
+        }, {
+          col: 1,
+          row: 3,
+          sizeY: 1,
+          sizeX: 1,
+          name: "Other Widget 2"
+        }]
+      }
+    };
+
+
+
+    $scope.resetAll = function(){
+      $scope.current_items = $scope.default_items;
+    };
+    $scope.saveCurrent = function(){
+      localStorage.setItem("saved_items", angular.toJson($scope.current_items));       
+    };
+
+
+
+
 
   });
